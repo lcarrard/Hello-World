ecd6753da33046ea8f006404851dd65caad9b4c0
lcarrard <carrard.laurent@bluemail.ch>
2021-02-07 15:23:14 +0100

    1ère version de my task

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'cle_secrete'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+db = SQLAlchemy(app)
+
+from routes import *   # rapporte l'ensemble du code dans routes-py
+
+if __name__ == "__main__":
+    app.run(debug=True)
